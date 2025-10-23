f88076938823392fe2fbd5b10585787c1eed0241
lazyyytushar <tusharesp444@gmail.com>
2025-10-23 11:04:29 +0530

    refactor: clean up unused imports and improve component structure

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,18 @@
-import { PlusIcon } from "lucide-react";
-import React from "react";
+import {  FilePenLineIcon, PlusIcon, UploadCloudIcon } from "lucide-react";
+import React, { useEffect } from "react";
+import { dummyResumeData } from "../assets/assets";
 
 const dashboard = () => {
+  const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
+
+  const [allResumes, setAllResumes] = useState([]);
+
+  const loadAllResumes = async () => {
+    setAllResumes(dummyResumeData);
+  };
+  useEffect(() => {
+    loadAllResumes;
+  }, []);
   return (
     <div>
       <div className="max-w-7xl mx-auto px-4 py-8">
@@ -9,10 +20,34 @@ const dashboard = () => {
           Welcome,Joe Doe
         </p>
         <div className="flex gap-4">
-          <button className="w-full">
-            <PlusIcon className="siz-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full" />
+          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer">
+            <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full" />
+            <p className=" text-sm group-hover:text-indigo-600 transition-all duration-300">
+              Create Resume
+            </p>
           </button>
-          <p className=" text-sm group-hover:text-indigo-600 transition-all duration-300">Create Resume</p>
+          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer">
+            <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-purple-300 to-purple-500 text-white rounded-full" />
+            <p className=" text-sm group-hover:text-purple-600 transition-all duration-300">
+              Upload Existing
+            </p>
+          </button>
+        </div>
+
+        <hr className="border-slate-300 my-6 sm:w-[305px]" />
+        <div>
+          {allResumes.map(() => {
+            const baseColor = colors[index % colors.length];
+            <button
+              key={index}
+              className="relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer"
+              style={{
+                background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40 )`,
+                borderColor: baseColor + "40",
+              }}>
+              <FilePenLineIcon />
+            </button>;
+          })}
         </div>
       </div>
     </div>
